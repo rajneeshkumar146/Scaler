a9c7cce3194e9e1c5c0d9ba3fcaf2107413eb2ac
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2023-09-29 09:16:42 +0530

    update

---
+++ Actual Class/02_Javascript/l002_this_oops/3_chaining.js
@@ -0,0 +1,44 @@
+// Statement 1.
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+    },
+    down() {
+        this.step--;
+    },
+    showStep: function () {
+        console.log(this.step);
+    }
+};
+
+// up fn on ladder
+ladder.up();  // step: 1
+
+// up fn on ladder
+ladder.up();  // step: 2
+
+ladder.up(); // step: 3
+ladder.down(); // step: 2
+ladder.showStep();  // 2
+
+
+// Statement 2.
+let ladder2 = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+};
+
+ladder2.up().up().up().down().showStep();
---
+++ Actual Class/02_Javascript/l002_this_oops/4_inheritance.js
@@ -0,0 +1,70 @@
+// statement 1 -------------------------------------------------
+
+let arr = [1, 2, 3, 4];
+
+console.log(typeof arr);
+
+arr.push("hello")
+console.log(arr);
+
+// arr ->[p] Array ->[p] Object->[p] null
+// Array -> all the method required by every array
+
+// using an inherited property
+console.log(arr.toString());
+
+// advantage  of  inheritance -> 
+// 1. reuse of the code
+// 2. saving memory space 
+
+
+//  classes in JS -> abstraction so that java dev can also 
+
+
+// statement 2 -------------------------------------------------
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4, 5, 6];
+
+
+// bad way of giving a feature************
+arr1.sum = function () {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+        sum = sum + this[i];
+    }
+    console.log(sum);
+}
+
+arr2.sum = function () {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+        sum = sum + this[i];
+    }
+    console.log(sum);
+}
+
+console.log(arr1);
+arr1.sum();
+arr2.sum();
+
+
+
+
+/************* parent ************/
+ Array.prototype.sum = function () {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+        sum = sum + this[i];
+    }
+    console.log(sum);
+}
+
+// usecase of this and prototype
+arr1.sum();
+arr2.sum();
+
+
+
+// i am admant -> i will keep on my own -> borrow for once 
+// bind , call ,apply
