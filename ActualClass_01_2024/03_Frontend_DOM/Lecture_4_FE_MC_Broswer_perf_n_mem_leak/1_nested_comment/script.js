6f9a34a0fb5112cb875afda0850e128a88094fc2
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-02-26 08:23:29 +0530

    HTML/CSS_Lecture_002

--- ActualClass_01_2024/03_Frontend_DOM/Lecture_4_FE_MC_Broswer_perf_n_mem_leak/1_nested_comment/script.js
+++ ActualClass_01_2024/03_Frontend_DOM/Lecture_4_FE_MC_Broswer_perf_n_mem_leak/1_nested_comment/script.js
@@ -3,28 +3,49 @@ const container = document.querySelector(".container")
 container.addEventListener("click", (e) => {
     const targetElem = e.target;
     if (targetElem.classList.contains("reply")) {
-        console.log("I'm in reply");
-
-        // <div class="comment_reply_container">
-        //      <input type="text" placeholder="Write your comment">
-        //      <button class="btn-submit">submit</button>
-        // </div> 
-
+        // console.log("I'm in reply");
         createReplyInput(e);
+    } else {
+        const isSubmit = targetElem.classList.contains("btn-submit");
+        if (isSubmit) {
+            createComment(e);
+        }
     }
 });
 
 
-function createReplyInput(e){
+function createReplyInput(e) {
     const fragment = document.createDocumentFragment();
     const replyContainer = document.createElement("div");
     const input = document.createElement("input");
     const button = document.createElement("button");
 
+    replyContainer.setAttribute("class", "comment_reply_container");
+
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Write your comment");
+
+    button.setAttribute("class", "btn-submit");
+    button.textContent = "submit";
 
     replyContainer.appendChild(input);
     replyContainer.appendChild(button);
     fragment.appendChild(replyContainer);
 
     e.target.parentNode.appendChild(fragment);
+}
+
+function createComment(e) {
+
+    // <div class="comment-container">
+    //             <div class="comment_card">
+    //                 <h3 class="coment_text">Thanks for asking, How are you?</h3>
+    //                 <div class="reply">Reply</div>
+    //             </div> 
+
+    const commentContainer = document.createElement("div");
+    commentContainer.setAttribute("class", "comment-container");
+
+    const input = e.target.parentNode.children[0];
+    // console.log(input.value);
 }
