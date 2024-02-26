0b06f98fa31393cdef5d5f59eed34db6089b8dbf
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-02-26 07:50:27 +0530

    HTML/CSS_Lecture_002

---
+++ ActualClass_01_2024/03_Frontend_DOM/Lecture_4_FE_MC_Broswer_perf_n_mem_leak/1_nested_comment/script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container")
+
+container.addEventListener("click", (e) => {
+    if (?????) {
+        console.log("I'm in reply");
+
+        // <div class="comment_reply_container">
+        //      <input type="text" placeholder="Write your comment">
+        //      <button class="btn-submit">submit</button>
+        // </div> 
+
+        createReplyInput(e);
+    }
+})
