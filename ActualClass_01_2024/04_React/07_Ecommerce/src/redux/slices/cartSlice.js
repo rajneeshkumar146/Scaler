544d35a26c8b2068a8548b9bbf123c0035cf165c
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-03-30 09:12:16 +0530

    update

--- ActualClass_01_2024/04_React/07_Ecommerce/src/redux/slices/cartSlice.js
+++ ActualClass_01_2024/04_React/07_Ecommerce/src/redux/slices/cartSlice.js
@@ -8,17 +8,40 @@ const cartSlice = createSlice({
         cartProducts: []
     },
     reducers: {
-        addTocart: (state, action) => {
+        addToCart: (state, action) => {
             state.cartQuantity++;
-            
+            const productToBeAdded = action.payload;
+            const requiredProduct = state.cartProducts.find((cProduct) => {
+                return cProduct.id == productToBeAdded.id;
+            });
 
+            if (requiredProduct != undefined) {
+                requiredProduct.indQuantity++;
+            } else {
+                // Not present in cart.
+                productToBeAdded.indQuantity = 1;
+                state.cartProducts.push(productToBeAdded);
+            }
         },
         deleteFromCart: (state, action) => {
-            state.cartQuantity--;
+            const productToBeAdded = action.payload;
+            const productIndex = state.cartProducts.findIndex((cProduct) => {
+                return cProduct.id == productToBeAdded.id;
+            });
 
+            if (productIndex != -1) {
+                state.cartQuantity--;
+                let product = state.cartProducts[productIndex];
+                if (product.indQuantity === 1) {
+                    state.cartProducts.splice(productIndex, 1);
+                } else {
+                    product.indQuantity--;
+                }
+            }
         }
     }
 
 });
 
+export const action = cartSlice.actions;
 export default cartSlice;
