f07259eee69f7a6858ec2aa8dbdd1dc57eb67f62
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-06-21 08:40:34 +0530

    update

---
+++ ActualClass_04_2024/04_Mern/03_MongoDb/index.js
@@ -0,0 +1,105 @@
+const mongoose = require("mongoose");
+const express = require("express")
+
+const app = express();
+app.use(express.json());
+
+const dbURL = "mongodb+srv://rajneeshkumar146:SAfmmRVnZLAIMmUk@cluster0.twfteet.mongodb.net/";
+
+// connect to DB.
+mongoose.connect(dbURL).then(function (connection) {
+    // console.log("Connected to MongoDB: ", connection);
+    console.log("Connected to MongoDB");
+}).catch(function (err) {
+    console.log("error: ", err);
+});
+
+
+/**
+   "product_name":"iPhone 19",
+   "product_price":"$2000",
+   "isInStock":true,
+   "category":"Phone",
+   "password":"123456789",
+   "confirmPassword":"123456789"
+ */
+
+// create a schema.
+const productSchema = new mongoose.Schema({
+    product_name: {
+        type: String,
+        required: true,
+    },
+    product_price: {
+        type: String,
+        required: true,
+    },
+    isInStock: {
+        type: Boolean,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: function () {
+                return this.password == this.confirmPassword;
+            },
+            message: "Password and confirm password should be same",
+        }
+    }
+});
+
+const productModel = mongoose.model("Products", productSchema);
+
+app.post("/api/products", async function (req, res) {
+    const body = req.body;
+    console.log("body: ", body);
+
+    try {
+        const product = await productModel.create({
+            product_name: body.product_name,
+            product_price: body.product_price,
+            category: body.category,
+            isInStock: body.isInStock,
+            password: body.password,
+            confirmPassword: body.confirmPassword
+        });
+
+        console.log("product: ", product);
+        return res.status(201).json({ message: "Product created successfully" });
+    } catch (err) {
+        console.log("error: ", err);
+        return res.status(400).json({ message: err.message });
+
+    }
+});
+
+app.get("/api/products", async function (req, res) {
+    // const allProducts = await productModel.find();
+    const allProducts = await productModel.find({ category: "TV" });
+
+    console.log("all products: ", allProducts);
+    return res.status(201).json(allProducts);
+});
+
+app.get("/api/products/:id", async (req, res) => {
+    const id = req.params.id;
+    const product = await productModel.findById(id);
+    res.status(200).json(product);
+});
+
+
+app.listen(3000, function () {
+    console.log("Server is running");
+})
---
+++ ActualClass_04_2024/04_Mern/03_MongoDb/notes.md
@@ -0,0 +1,15 @@
+1. Sin in/up : https://www.mongodb.com/products/platform/atlas-database
+2. Create your new project: <Name of your project>
+3. Create cluster
+    3.1 choose free one.
+    3.2 Choose AWS as provider.
+    3.3 Choose region <In my case it is mumbai>
+    3.4 Click on create cluster.
+4. After creating cluster don't forget to URL from VS code Integration which include userName and Password.
+4. Under security section:
+   4.1 Network access
+   4.2 Add IP address: <Your IP address>
+        4.2.1 Choose <allow access from any where>
+
+
+Reference: https://mongoosejs.com/docs/schematypes.html
