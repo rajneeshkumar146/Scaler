4b6cfd780e9ba06122962db3cd703c5642641a37
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-06-28 08:03:12 +0530

    update

--- ActualClass_04_2024/04_Mern/05_BookMyShow_/client/src/api/users.js
+++ ActualClass_04_2024/04_Mern/05_BookMyShow_/client/src/api/users.js
@@ -1,15 +1,6 @@
 const {axiosInstance} = require(".");
 
 
-export const RegisterUser = async (value) => {
-    try{
-        const response = await axiosInstance.post("api/users/register",value);
-        return response.data;
-    }catch(err){
-        console.log("Error occured: ", err);
-    }
-}
-
 export const LoginUser = async (value) => {
     try{
         const response = await axiosInstance.post("api/users/login",value);
